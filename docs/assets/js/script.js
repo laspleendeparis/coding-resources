85a85d688f1111f10ef6c33d1f3dae6a605a169c
badmagick329 <uzairfarooqi@gmail.com>
2025-03-23 15:37:21 +0000

    fix search
    using data attributes to grab searchables

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -8,18 +8,7 @@ function main() {
  * @returns {NodeListOf<HTMLAnchorElement>[]} An array containing all link elements from the three sections
  */
 function getListItems() {
-  const resourceListItems = document.querySelectorAll("#resource-list>li>a");
-  const generalProgrammingItems = document.querySelectorAll(
-    "#general-programming>li>a"
-  );
-  const hardwareResourceListItems = document.querySelectorAll(
-    "#hardware-resource-list>li>a"
-  );
-  return [
-    ...resourceListItems,
-    ...generalProgrammingItems,
-    ...hardwareResourceListItems,
-  ];
+  return document.querySelectorAll("[data-searchable]");
 }
 
 /**
